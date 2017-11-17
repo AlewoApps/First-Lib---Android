ca2038824c83ff765d1c64b165367167d4a11643
Farkas Sándor <farkas.sanyi180@gmail.com>
2017-11-17 13:06:13 +0100

    Created the Lib

---
+++ firstlib/src/main/java/com/alewo/firstlib/FirstLib.java
@@ -0,0 +1,15 @@
+package com.alewo.firstlib;
+
+/**
+ * Created by alewo on 2017.11.17..
+ */
+
+public class FirstLib {
+
+    public FirstLib() {
+    }
+
+    public String asd() {
+        return "Hello from your first lib motherfucker";
+    }
+}
